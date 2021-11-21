aa68a2a928122a82eeb88998216f73e3bb25e57d
Rachel <rdalby@hsph.harvard.edu>
2021-11-21 18:53:04 -0500

    first commit

---
+++ Assets/Scripts/GameOverText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverText : MonoBehaviour {
+
+	private Text text;
+
+	void OnTriggerEnter(Collider other) {
+		GameObject wonTxt = GameObject.Find("GameOver");
+		text = wonTxt.GetComponent<Text>();
+		
+		text.color = new Color(0, 0, 0, 1);
+		text.text = "YOU WON!";
+			
+
+	}
+}
---
+++ Assets/Scripts/YouWonText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class YouWonText : MonoBehaviour {
+
+	private Text text;
+		// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+
+		// start text off as completely transparent black
+		text.color = new Color(0, 0, 0, 0);
+	}
+
+}
